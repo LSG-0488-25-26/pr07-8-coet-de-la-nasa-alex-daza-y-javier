a05c588c7b7c9e1fd8e0290e1d0945608569364d
javier.grima@gracia.lasalle.cat <javier.grima@gracia.lasalle.cat>
2026-01-22 16:53:22 +0100

    implementaciones

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,13 +5,13 @@ plugins {
 }
 
 android {
-    namespace = "com.example.api_videojuegos"
+    namespace = "com.example.lazycomponents"
     compileSdk {
         version = release(36)
     }
 
     defaultConfig {
-        applicationId = "com.example.api_videojuegos"
+        applicationId = "com.example.lazycomponents"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
@@ -50,6 +50,10 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
